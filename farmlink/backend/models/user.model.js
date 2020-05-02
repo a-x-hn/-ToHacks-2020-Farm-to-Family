1972c9299d12585db2b0f4474b9e2e94db8f3dea
alexhan <alexhan0324@gmail.com>
2020-05-02 11:29:45 -0700

    a

--- farmlink/backend/models/user.model.js
+++ farmlink/backend/models/user.model.js
@@ -1,4 +1,8 @@
-const userSchema = new userSchema({
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
     username: {
         type: String,
         required: true,

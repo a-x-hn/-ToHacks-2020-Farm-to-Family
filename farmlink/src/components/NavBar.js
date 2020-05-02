b27756458dcc56b232e5958bad72b01f965f08cb
leon-han19 <leon.han2001@gmail.com>
2020-05-02 17:33:32 -0400

    navbar ui

--- farmlink/src/components/NavBar.js
+++ farmlink/src/components/NavBar.js
@@ -1,1 +1,23 @@
 import React from "react";
+class Navbar extends React.Component {
+    render() {
+      return (
+          <div class="ui secondary  menu">
+              <Link to="/">User-landingpage</Link>
+              <a class="item">
+                  Farmlink
+              </a>
+              <div class="right menu">
+                  <a class="item">
+                      Current Order
+                  </a>
+                  <a class="item active">
+                      Your account
+                  </a>
+              </div>
+          </div>
+      );
+    }
+  }
+  
+  export default Navbar;

afe23a80860320dc80ef699b54695aef88d9fb92
alexhan <alexhan0324@gmail.com>
2020-05-02 12:20:52 -0700

    added subdoc productSchema

--- farmlink/backend/models/farmer.model.js
+++ farmlink/backend/models/farmer.model.js
@@ -2,6 +2,24 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+// Improvements to Price Inputs
+
+function getPrice(num) {
+    return (num/100).toFixed(2);
+}
+
+function setPrice(num) {
+    return num*100;
+}
+
+// Subdocument for products
+
+const productSchema = new Schema({
+    name: { type: String, required: true },
+    qty: { type: Number, required: true },
+    price: { type: Number, get: getPrice, set: setPrice }
+})
+
 const farmerSchema = new Schema({
     username: {
         type: String,
@@ -10,13 +28,16 @@ const farmerSchema = new Schema({
         trim: true,
         minlength: 3
     },
-    email: { type: String, required: true},
-    password: {},
-    phone: {},
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
     firstname: { type: String, required: true },
     lastname: { type: String, required: true },
-    middlename: { type: String, required: true },
-    address: {},
+    middlename: { type: String, required: false },
+    address: { type: String, required: true },
+    currentlistings: [ productSchema ],
+    pastlistings: [ productSchema ],
+    pendinglistings: [ productSchema ]
 }, {
     timestamps: true,
 });

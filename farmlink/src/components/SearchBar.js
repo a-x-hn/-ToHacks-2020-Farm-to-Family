8bee5b96a2aab3a3af9c84863b325c57dce8ee08
Andrew Ma <ayma01@gmail.com>
2020-05-02 16:36:14 -0600

    Updating search functionality

--- farmlink/src/components/SearchBar.js
+++ farmlink/src/components/SearchBar.js
@@ -1,15 +1,29 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  state = {
-    inputValue: "",
+  constructor(props) {
+    super(props);
+    // Don't call this.setState() here!
+    this.state = { inputValue: "" };
+  }
+
+  searchChange = (event) => {
+    console.log("change", event.target.value);
+    this.setState({
+      inputValue: event.target.value,
+    });
   };
 
   render() {
     return (
       <div className="ui category search">
         <div className="ui icon input">
-          <input className="prompt" type="text" placeholder="Search Goods..." />
+          <input
+            className="prompt"
+            onChange={this.searchChange}
+            type="text"
+            placeholder="Search Goods..."
+          />
           <i className="search icon"></i>
         </div>
         <div className="results"></div>

c301382a9bd1d106fb12e1454fb1199f33d99013
alexhan <alexhan0324@gmail.com>
2020-05-02 11:27:54 -0700

    Update

---
+++ farmlink/backend/models/user.model.js
@@ -0,0 +1,15 @@
+const userSchema = new userSchema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3
+    },
+}, {
+    timestamps: true,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;

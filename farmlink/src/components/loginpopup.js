a18a3855d7f4635116cc123b65a40789cd32fbba
Andy Lee <andylee.mk@gmail.com>
2020-05-02 20:09:29 -0700

    test commit

---
+++ farmlink/src/components/loginpopup.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class LogInPopUp extends React.Component {
+    render() {
+      return <h2>Test 2</h2>;
+    }
+  }
+
+  ReactDOM.render(<LogInPopUp />, document.getElementById('root'));
